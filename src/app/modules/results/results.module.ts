ad6474922ca379b5fe52c4de58f5dec326f26c43
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2020-03-27 14:13:41 -0400

    Custom fonts added

---
+++ src/app/modules/results/results.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResultsMainComponent } from './results-main/results-main.component';
+
+
+
+@NgModule({
+  declarations: [ResultsMainComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ResultsModule { }
