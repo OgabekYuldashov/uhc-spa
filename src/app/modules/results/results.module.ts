9c0a107f14c38e5b68fbf1e11c1f82c9839e3283
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2020-03-28 11:16:30 -0400

    Website link in search result item fixed and some improvements

--- src/app/modules/results/results.module.ts
+++ src/app/modules/results/results.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultsMainComponent } from './results-main/results-main.component';
+import { ResultItemComponent } from './result-item/result-item.component';
+import { MapComponent } from './map/map.component';
 
 
 
 @NgModule({
-  declarations: [ResultsMainComponent],
+  declarations: [ResultsMainComponent, ResultItemComponent, MapComponent],
   imports: [
     CommonModule
   ]
