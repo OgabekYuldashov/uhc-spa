5baed9c19860448b7e848818921d39e3defe08cf
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2020-03-28 14:42:33 -0400

    Map working with hardcoded values. DataService file created

--- src/app/modules/results/results-main/results-main.component.ts
+++ src/app/modules/results/results-main/results-main.component.ts
@@ -1,15 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-results-main',
   templateUrl: './results-main.component.html',
   styleUrls: ['./results-main.component.css']
 })
-export class ResultsMainComponent implements OnInit {
+export class ResultsMainComponent implements OnInit, AfterViewInit {
+  @ViewChild('resultsMap', {static: false}) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.730610;
+  lng = -73.935242;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8,
+  };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
+
+  markers = [
+    {
+      position: new google.maps.LatLng(38.75948077253505, -75.1281763573177),
+      map: this.map,
+      title: 'Marker 1'
+    },
+    {
+      position: new google.maps.LatLng(38.762026, -75.14523),
+      map: this.map,
+      title: 'Marker 2'
+    },
+    {
+      position: new google.maps.LatLng(38.722522, -75.1197165),
+      map: this.map,
+      title: 'Marker 3'
+    }
+  ];
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.mapInitializer();
+  }
+
+  mapInitializer(): void {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+
+    // Adding Click event to default marker
+    this.marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: this.marker.getTitle()
+      });
+      infoWindow.open(this.marker.getMap(), this.marker);
+    });
+
+    // Adding default marker to map
+    this.marker.setMap(this.map);
+
+    // Adding other markers
+    this.loadAllMarkers();
+  }
+
+  loadAllMarkers(): void {
+    this.markers.forEach(markerInfo => {
+      // Creating a new marker object
+      const marker = new google.maps.Marker({
+        ...markerInfo
+      });
+
+      // creating a new info window with markers info
+      const infoWindow = new google.maps.InfoWindow({
+        content: marker.getTitle()
+      });
+
+      // Add click event to open info window on marker
+      marker.addListener('click', () => {
+        infoWindow.open(marker.getMap(), marker);
+      });
+
+      // Adding marker to google map
+      marker.setMap(this.map);
+    });
   }
 
 }
