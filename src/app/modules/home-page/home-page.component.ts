e63bb9cb96437cab32ab3750cf42e91470511911
Wuletaw Adane Tefera <49078588+WuleKop@users.noreply.github.com>
2020-03-29 16:47:30 -0400

    Details View

--- src/app/modules/home-page/home-page.component.ts
+++ src/app/modules/home-page/home-page.component.ts
@@ -109,7 +109,6 @@ export class HomePageComponent implements OnInit, AfterViewInit {
     this.isAdvancedSearchButtonCliked = false;
 
     // this.dataSearchService.getDummyRecords(this.parameter_list)
-
     this.dataSearchService.setParameters(this.parameter_list);
     this.route.navigate((['results']));
 
