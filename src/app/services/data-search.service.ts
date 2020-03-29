d2acd15d2c9c8d632c0b624b8dc6d49e563f1c60
Fadejimi Adegbulugbe <adegbulugbefadejimi@gmail.com>
2020-03-28 23:16:59 -0400

    data service basic

--- src/app/services/data-search.service.ts
+++ src/app/services/data-search.service.ts
@@ -1,18 +1,72 @@
 import { Injectable } from '@angular/core';
 import {ResultItem} from '../models/ResultItem';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {RequestParams} from 'elasticsearch';
+import {JsonObject} from '@angular/compiler-cli/ngcc/src/packages/entry_point';
 
+const headers = new HttpHeaders({
+  'Content-Type': 'application/json'
+  // , Authorization: 'Basic ' + btoa('elastic:altimetrik')
+});
 @Injectable({
   providedIn: 'root'
 })
 export class DataSearchService {
+  index = 'uhc_v4';
+  hostUrl: any = `http://localhost:9200/${this.index}/_search`;
+  // hostUrl: any = `http://34.95.36.224:9200/${this.index}/`;
+  user: any = 'elastic';
+  password: any = 'changeme';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.connect();
+  }
+
+  connect() {
+  }
 
   getDummyRecords(): ResultItem[] {
+    const searchParams: RequestParams.Search = {
+        query: {
+          match: { city: 'chicago' }
+        }
+    };
+    const results: Array<ResultItem> = new Array<ResultItem>();
+    const query0: RequestParams.Search = {
+      query: {
+        bool : {
+          must: {
+            match: {
+              city: 'chicago'
+            }
+          },
+          filter : {
+            geo_distance : {
+              distance : "200km",
+              location : "40,-70"
+            }
+          }
+        }
+      }
+    };
 
-    const item = new ResultItem(
-    );
-
+    const query: RequestParams = {
+      size: 10,
+      from: 0
+    };
+    // @ts-ignore
+    this.httpClient.post<JsonObject>(this.hostUrl, query, headers).subscribe(s => {
+      const j = s;
+      // @ts-ignore
+      for (const k of j.hits.hits) {
+        // console.log(k);
+        let res = new ResultItem();
+        // console.log(k._source);
+        res = k._source;
+        results.push(res);
+      }
+      console.log(results);
+    });
 
     return null;
   }
